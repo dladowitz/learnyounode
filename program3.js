a265bae91e0aaf43386f39dbe210dd3954d59922
David Ladowitz <david@ladowitz.com>
2015-04-28 10:44:43 -0700

    finished level 3

--- program3.js
+++ program3.js
@@ -1,15 +1,9 @@
-var fs = require('fs');
-var file_path = process.argv[2];
-// console.log(file_path)
+var fs = require('fs')
+var filePath = process.argv[2]
 
-var buffered_file_contents = fs.readFileSync(file_path);
-// console.log(buffered_file_contents)
+var buffer = fs.readFileSync(filePath)
 
-var stringed_file_contents = buffered_file_contents.toString();
-// console.log(stringed_file_contents)
+var stringBuffer = buffer.toString()
 
-var lines_array = stringed_file_contents.split("\n");
-// console.log(lines_array)
-
-var num_of_lines = lines_array.length;
-console.log(num_of_lines - 1);
+var numOfLines = stringBuffer.split("\n").length - 1
+console.log(numOfLines)
