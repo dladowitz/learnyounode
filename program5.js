0da8234e1fb9c7f13837c1b65802303ee443d404
David Ladowitz <david@ladowitz.com>
2015-04-28 13:38:53 -0700

    finishingg level 5

--- program5.js
+++ program5.js
@@ -1,32 +1,15 @@
-var fs = require('fs');
-var directory_path = process.argv[2];
-var file_type = process.argv[3];
-var debug = process.argv[4];
+var fs = require("fs")
+var path = require('path')
+var dirPath = process.argv[2]
+var extenstion = "." + process.argv[3]
 
-if (debug) console.log("file type: " + file_type);
-
-var regex = new RegExp(".*\." + file_type + "$");
-if (debug) console.log("regex " + regex);
-
-function printError(error){
-  console.log("-----------------ERROR-----------------");
-  console.log(error);
-  console.log("-----------------ERROR-----------------");
+function callback (err, list) { 
+  list.forEach(function(file){
+    if(path.extname(file) === extenstion){
+      console.log(file)
+    }
+  })
 }
 
-fs.readdir(directory_path, function callback (err, list){
-  if(err){
-    printError(err);    
-  }else{
-    if (debug) console.log("list: " + list);
 
-    list.forEach(function(file){
-      if (debug) console.log("file: " + file);
-
-      if(file.match(regex)){
-        if (debug) console.log("matched!");        
-        console.log(file);
-      }
-    })
-  }
-})
+fs.readdir(dirPath, callback)
