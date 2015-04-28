ff505d9740b44477257e62c540e4f3741019819b
David Ladowitz <david@ladowitz.com>
2015-04-28 15:39:34 -0700

    finishing level 8

--- program7.js
+++ program7.js
@@ -1,7 +1,7 @@
 var http = require("http");
 var url = process.argv[2];
 
-http.get(url, function callback (response) { 
+http.get(url, function callback(response) { 
   // console.log('STATUS: ' + response.statusCode);
   // console.log('HEADERS: ' + JSON.stringify(response.headers));
   response.setEncoding('utf8');
